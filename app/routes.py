2685f0a8379b13cf0313b715be3af28d30559db4
hmani3 <heemanijaz@gmail.com>
2025-01-16 14:16:11 -0500

    fix route for scraping

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, render_template, request
+from .scraper import scrape_news
 
 main = Blueprint('main', __name__)
 
-@main.route('/home')
+@main.route('/')
 def home():
     return render_template('index.html')
 
-# @main.route('/scrape', methods=['POST'])
-# def scrape():
-#     url = request.form['url']
-#     news = scrape_news(url)
-#     return render_template('results.html', news=news)
+@main.route('/scrape', methods=['POST'])
+def scrape():
+     url = request.form['url']
+     news = scrape_news(url)
+     return render_template('result.html', news=news)
