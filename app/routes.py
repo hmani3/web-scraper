e2d70450bf8bca172109ff85331134785cf442f2
hmani3 <heemanijaz@gmail.com>
2025-01-17 09:53:23 -0500

    add functionality for scraping practice sites

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from .scraper import scraped_sites
+from .scraper import scraped
 
 main = Blueprint('main', __name__)
 
@@ -10,9 +10,9 @@ def home():
 @main.route('/scrape', methods=['POST'])
 def scrape():
      url = request.form['url']
-     jobs = scraped_sites(url)
-     jobs = [
-        {"title": "Software Engineer", "company": "TechCorp", "location": "Remote", "salary": "$100,000", "link": "https://example.com/job1"},
-        {"title": "Data Scientist", "company": "Data Inc.", "location": "New York, NY", "salary": "$120,000", "link": "https://example.com/job2"},
-    ]
-     return render_template('result.html', jobs=jobs)
+     result = scraped(url)
+    #  jobs = [
+    #     {"title": "Software Engineer", "company": "TechCorp", "location": "Remote", "salary": "$100,000", "link": "https://example.com/job1"},
+    #     {"title": "Data Scientist", "company": "Data Inc.", "location": "New York, NY", "salary": "$120,000", "link": "https://example.com/job2"},
+    # ]
+     return render_template('result.html', result=result)
